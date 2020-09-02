5eb597e88e3e43f6bdf9a5f0e9b34d3d4d5cf611
Emerson Enright <emersonenright@Emersons-MacBook-Pro.local>
2020-09-02 01:42:21 -0700

    Updates

---
+++ precache-manifest.9c7fa592a46a7a5541d3d1af3ea39a53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c439ec9e2acd3d940988dd0ba89155c8",
+    "url": "/buttterfly-app/index.html"
+  },
+  {
+    "revision": "6989ec87075738e07cbe",
+    "url": "/buttterfly-app/static/css/main.b478e3ea.chunk.css"
+  },
+  {
+    "revision": "e385dcaf903d03da5e94",
+    "url": "/buttterfly-app/static/js/2.03ece3c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buttterfly-app/static/js/2.03ece3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6989ec87075738e07cbe",
+    "url": "/buttterfly-app/static/js/main.24d592be.chunk.js"
+  },
+  {
+    "revision": "32ec712da664ba07c0f9",
+    "url": "/buttterfly-app/static/js/runtime-main.c4e2f026.js"
+  },
+  {
+    "revision": "d5b6df7d782b4a8c2b2827a6a01f4ace",
+    "url": "/buttterfly-app/static/media/ Butterfly2.d5b6df7d.jpg"
+  },
+  {
+    "revision": "201efce68dd9d56f0881ec74d04be3fc",
+    "url": "/buttterfly-app/static/media/Butterfly1.201efce6.jpg"
+  },
+  {
+    "revision": "458e049494e1b4951d26faac72f2734b",
+    "url": "/buttterfly-app/static/media/Butterfly3.458e0494.jpg"
+  }
+]);
