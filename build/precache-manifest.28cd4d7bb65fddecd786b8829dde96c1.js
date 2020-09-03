2c3ab99e329f2e05328bb63e9c54e3ad156e1b4b
ebe2113 <56198651+ebe2113@users.noreply.github.com>
2020-09-03 10:47:19 -0700

    Add files via upload

---
+++ build/precache-manifest.28cd4d7bb65fddecd786b8829dde96c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2483c682a4c05cc72527a8bb9aa3dcc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05fe023ee86edea30837",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "599cf20061518c12393f",
+    "url": "/static/js/2.9236d55f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9236d55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fe023ee86edea30837",
+    "url": "/static/js/main.769546bd.chunk.js"
+  },
+  {
+    "revision": "28ed903c4bcd313d2347",
+    "url": "/static/js/runtime-main.fa25140a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
